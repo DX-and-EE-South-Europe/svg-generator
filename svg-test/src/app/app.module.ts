aa07c22703f9ddd6e9648c8f0b63fd6b6c3b7304
Jaime Martin Martin <jaime.martinmartin@cognizant.com>
2022-05-12 16:35:09 +0200

    Implement demo after using builder

--- svg-test/src/app/app.module.ts
+++ svg-test/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FruitsSvgModule } from 'src/generated-svg/fruits-svg.module';
+import { ShapesSvgModule } from 'src/generated-svg/shapes-svg.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShapesSvgModule,
+    FruitsSvgModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
